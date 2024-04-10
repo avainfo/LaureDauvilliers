d931910691d2b877c8653771ce52d76598b2b928
BuildTools <unconfigured@null.spigotmc.org>
2024-04-10 10:21:40 +0200

    feat[HomePage]: Created header

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/pages/App.scss';
+import Header from "../styles/components/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header/>
     </div>
   );
 }
---
+++ src/styles/components/Header.tsx
@@ -0,0 +1,19 @@
+import "../../components/homepage/header.scss"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="topnav">
+                <a href="/App">La sophrologie</a>
+                <a href="/App">L’hypnose</a>
+                <a href="/App">Séances</a>
+                <a href="/App">Contact</a>
+            </div>
+            <div className="title">
+                    <h2>Laure</h2>
+                    <h1>DAUVILLIERS</h1>
+                    <h3>Sophrologue</h3>
+            </div>
+        </div>
+    );
+}
